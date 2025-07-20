fce73cfbdf18a4f00b6c2cd7f1e0b85aee89b859
Ｄʜʀᴜᴠᴀ Ｋᴜᴍᴀʀ <ffg288844@gmail.com>
2025-07-20 16:26:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def handle_requests():
     if not uid or not server_name or not key:
         return jsonify({"error": "UID, region, and key are required"}), 400
 
-    if key != "2yearkeysforujjaiwal":
+    if key != "1yearskeysforujjaiwal":
         return jsonify({"error": "Invalid API key"}), 403
 
     try:
